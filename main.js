61bee6ec45494dad2fe4104b0aa35cf98de48374
Ethan Simpson <ethansown@gmail.com>
2018-11-07 19:13:00 -0600

    Added Gradient to drawCircle();

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
+
 function drawLine(){
-var canvas = document.getElementById('mainCanvas');
-var canvasLine = canvas.getContext("2d");
 canvasLine.moveTo(0,0);
 canvasLine.lineTo(400, 200);
 canvasLine.stroke();
@@ -8,7 +7,15 @@ canvasLine.stroke();
 function drawCircle(){
 var canvas = document.getElementById('mainCanvas');
 var canvasLine = canvas.getContext("2d");
+
+let gradient = canvasLine.createLinearGradient(0,0,200,0);
+gradient.addColorStop(0, "red");
+gradient.addColorStop(1, "blue");
+
+canvasLine.fillStyle=gradient;
+canvasLine.fillRect(0, 0, 400, 200);
+
 canvasLine.beginPath();
-canvasLine.arc(150, 75, 20, 0, 10);
+canvasLine.arc(150, 75, 40, 0, 20);
 canvasLine.stroke();
 }
