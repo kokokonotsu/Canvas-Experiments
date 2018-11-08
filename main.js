b7c9ca70a401fff6dca501858c9d50767a7e5839
Ethan Simpson <ethansown@gmail.com>
2018-11-07 19:47:32 -0600

    What have I done

--- main.js
+++ main.js
@@ -16,15 +16,21 @@ canvasLine.stroke();
 function drawCircle(){
 var canvas = document.getElementById('mainCanvas');
 var canvasLine = canvas.getContext("2d");
-
+var canvasImage = document.getElementById("canvasImage");
+let canvasPattern = canvasLine.createPattern(canvasImage, 'no-repeat');
 let gradient = canvasLine.createRadialGradient(100,50,5,100,70,50);
 gradient.addColorStop(0, "red");
-gradient.addColorStop(1, "blue");
+gradient.addColorStop(1, "white");
 
 canvasLine.fillStyle=gradient;
 canvasLine.fillRect(0, 0, 400, 200);
 
+//Creates Circle on-top of gradient;
 canvasLine.beginPath();
-canvasLine.arc(150, 75, 40, 0, 20);
+canvasLine.arc(120, 100, 50, 0, 2*Math.PI/*Or Integer 20*/); 
 canvasLine.stroke();
+
+//Fills circle with 'canvasPattern' pattern// 
+canvasLine.fillStyle=canvasPattern; 
+canvasLine.fill();
 }
